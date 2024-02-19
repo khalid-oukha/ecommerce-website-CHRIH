0dbe9a287958237cfcafb72e393dd715ffc1c1a7
adnaneoth <adnaneothmane64@gmail.com>
2024-02-19 11:40:21 +0100

    add auth and reset password

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
-<?php
-
+<?php 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,8 +11,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -17,7 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::get('/register', [AuthController::class, 'index'])->name('register');
 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+
+Route::post('/register', [AuthController::class, 'register'])->name('newregister');
+Route::post('/login', [AuthController::class, 'login'])->name('newlogin');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', function () { return view('dashboard'); });
+
+
+
+
+Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
+Route::post('/forgot-password', [AuthController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('/reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('password.reset');
+Route::post('/reset-pasAsword', [AuthController::class, 'resetPassword'])->name('password.update');
