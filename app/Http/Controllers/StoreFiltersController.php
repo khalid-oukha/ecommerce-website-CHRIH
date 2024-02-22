87f92c120dc9a671968acde5556f8fe8d3edb26b
khalid oukha <oukhakhalid@gmail.com>
2024-02-22 09:33:46 +0100

    updated home page with routes

--- app/Http/Controllers/StoreFiltersController.php
+++ app/Http/Controllers/StoreFiltersController.php
@@ -12,12 +12,9 @@ class StoreFiltersController extends Controller
     {
         $categorieId = $request->input('categorie');
         
-        // Add logic to retrieve products based on the selected category ($categorieId)
-        // For example, assuming your Product model has a 'category_id' column:
         $products = Product::where('categorie_id', $categorieId)->get();
-        
-        // You can return the products as JSON or however you prefer to structure the response
         return response()->json(['products' => $products]);
+
     }
     
 }
